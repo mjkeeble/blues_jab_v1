ada223de7a339b4dca6586b7879ede8d37a302e4
Mark Keeble <mjkeeble@yahoo.de>
2024-10-20 13:19:24 +0200

    add media type definitions for json-server

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,27 @@
 export type Translation = {
-  en: string;
-  de: string;
+	en: string;
+	de: string;
 };
 
 export type Gig = {
-  index: number;
-  venue: string;
-  town: string;
-  dateTime: string;
-  comment?: Translation;
-  mapUrl: string;
+	index: number;
+	venue: string;
+	town: string;
+	dateTime: string;
+	comment?: Translation;
+	mapUrl: string;
 };
 
 export type Member = {
-  name: string;
-  image: string;
+	name: string;
+	image: string;
+};
+
+export type Photo = {
+	fileId: string;
+};
+
+export type Video = {
+	title: string;
+	urlRef: string;
 };
